e2769d3119dc242dc599269dbdfad5857f947d1f
Pablo Elgueta Truffello (CELA) <t-paelgu@microsoft.com>
2017-06-09 14:03:23 -0400

    nacho culiao

--- app/dialogs/informaciongeneral.js
+++ app/dialogs/informaciongeneral.js
@@ -1,3 +1,9 @@
+var builder = require('botbuilder'); //usar libreria botbuilder
+var restify = require('restify'); //usar libreria restify webservice
+
 module.exports = function (session) {
-       session.send('Estas en Información General');
+    
+    builder.Prompts.choice(session, 'Tengo información sobre los siguientes temas:', 'Legal|Comercial|Recursos Humanos|Nuevo Empleado|Otros', { listStyle: builder.ListStyle.button });
+},
+    function (session, results) {
     }
