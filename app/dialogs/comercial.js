f5be7d60d2c24cb0a774f2c216a247c8bd3801f3
Pablo Elgueta Truffello (CELA) <t-paelgu@microsoft.com>
2017-08-11 16:08:29 -0400

    replace dialog

--- app/dialogs/comercial.js
+++ app/dialogs/comercial.js
@@ -9,19 +9,19 @@ module.exports = ([function (session) {
             switch (results.response.entity) {
                 case 'Lista de Precios':
                     session.send('Puedes encontrar las lista de precios de cada tipo de contrato en el siguiente link: https://aka.ms/listaprecios. De no tener acceso, deberás solicitarlo siguiendo la instrucción entregada en el sharepoint.');
-             
+                    session.replaceDialog("/Comercial");
                     break;
                 case 'Pedir Descuentos':
                     session.send('Para pedir descuentos especificos o programáticos en algun producto debes ingresar a http://bcweb.microsoft.com y generar un "exception request". Si es tu primera vez ingresando a la tool deberás pedir acceso ingresando a http://idweb/identitymanagement/aspx/groups/AllGroups.aspx y unirte al SG (Security Group) "BCWEB_User Access". Si necesitas mayor información respecto a cómo generar un request de bcweb, puedes descargar el siguiente documento https://aka.ms/bcwebguide y utilizarlo cómo guía.');
-
+                    session.replaceDialog("/Comercial");
                     break;
                 case 'Pedir BIF':
                     session.send('Para pedir un Business Investment Fund debes ingresar a http://getbif y generar la solicitud.');
-                 
+                    session.replaceDialog("/Comercial");
                     break;
                 case 'PO':
                     session.send('Para hacer una orden de compras debes ingresar a https://myorder.');
-       
+                    session.replaceDialog("/Comercial");
                     break;
         }
 
